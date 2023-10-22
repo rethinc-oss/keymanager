8ea9fd6a348ba7b7c6b97bc31a18b403cdd31f09
Tim Kettler <tim.kettler@rethinc.de>
2023-10-22 15:21:53 +0200

    Implement initial version of the init command

---
+++ lib/keymanager_helpers.sh
@@ -0,0 +1,61 @@
+# https://stackoverflow.com/a/28393320
+read_secret()
+{
+    # Save settings and disable echo.
+    current_tty="$(stty -g)"
+    stty -echo
+
+    # Read secret.
+    read "$@"
+
+    # Restore settings.
+    stty "$current_tty"
+    unset current_tty
+
+    # Print a newline because the newline entered by the user after
+    # entering the passcode is not echoed. This ensures that the
+    # next line of output begins at a new line.
+    echo
+}
+
+fn_cleanup() {
+    yellowprint "Cleaning up:"
+    if [ "${current_tty+1}" ]; then
+        yellowprint "- Restoring tty settings"
+        stty "$current_tty"
+        unset current_tty
+    fi
+
+    yellowprint "Stopping GPG-Agent..."
+    gpgconf --homedir /tmp/gpghome --kill gpg-agent; sleep 3
+
+    if mount | grep -q "$MOUNT_PATH"; then
+      yellowprint "Unmounting GPGHOME..."
+      umount $MOUNT_PATH && rm -rf $MOUNT_PATH
+    fi
+
+    if [ -e /dev/mapper/GPGHOME ]; then
+        yellowprint "Closing LUKS container..."
+        cryptsetup close GPGHOME
+    fi
+}
+
+fn_read_container_password() {
+    if [ ! "${CONTAINER_PASSWD+1}" ]; then
+        # Cache container password
+        printf "Password for container: "
+        read_secret CONTAINER_PASSWD
+    fi
+}
+
+fn_open_container() {
+    if cryptsetup isLuks $CONTAINER_PATH; then
+        echo $CONTAINER_PASSWD | cryptsetup open --type=luks2 $CONTAINER_PATH GPGHOME
+        mkdir -p $MOUNT_PATH
+        mount -t ext2 /dev/mapper/GPGHOME /$MOUNT_PATH
+        chmod 700 $MOUNT_PATH
+    else
+        redprint "The file is not a valid LUKS container."
+        exit 1
+    fi
+}
--- src/init.sh
+++ src/init.sh
@@ -1,12 +1,45 @@
 fn_init() {
-    echo "PARAM: $NAME"
-    echo "PARAM: $EMAIL"
+    CALLING_USER=${SUDO_USER:-$USER}
 
-    redprint "TEST"
+    if [ -z "$NAME" ]; then
+        redprint "Must give your name"
+        exit 1;
+    fi
 
-    i=0
-    while [ $# -gt 0 ] && i=$((i + 1)); do
-        echo "$i: $1"
-        shift
-    done
+    if [ -z "$EMAIL" ]; then
+        redprint "Must give your email"
+        exit 1;
+    fi
+
+    # Set up trap to ensure settings are restored before exiting
+    current_traps=$(trap)
+    trap 'fn_cleanup' EXIT HUP INT TERM
+
+    # Read & cache container password
+    fn_read_container_password
+
+    # Create encrypted container, if it not exists
+    if [ ! -e $CONTAINER_PATH ]; then
+        greenprint "Creating encrypted container file..."
+        dd if=/dev/urandom of=$CONTAINER_PATH bs=100M count=1 iflag=fullblock status=none
+        chown $CALLING_USER:$CALLING_USER $CONTAINER_PATH
+        echo $CONTAINER_PASSWD | cryptsetup --type=luks2 --label='GPGHOME' --pbkdf pbkdf2 --batch-mode luksFormat $CONTAINER_PATH
+        echo $CONTAINER_PASSWD | cryptsetup open --type=luks2 $CONTAINER_PATH GPGHOME
+        mkfs.ext2 -q /dev/mapper/GPGHOME
+        sync; sleep 2
+        cryptsetup close GPGHOME
+    else
+        greenprint "Container file exists, skipping creation..."
+    fi
+
+    greenprint "Opening encrypted container file..."
+    fn_open_container
+
+    # Create new GPG Masterkey, if it not exists
+    if ! gpg --quiet --homedir=$MOUNT_PATH --list-secret-keys --with-colons | read REPLY; then
+        greenprint "Generating GPG Masterkey"
+        gpg --quiet --homedir=$MOUNT_PATH --quick-gen-key "${NAME} <${EMAIL}>" ed25519 cert 50y
+    else
+        greenprint "GPG Masterkey exists, skipping creation..."
+    fi
 }
