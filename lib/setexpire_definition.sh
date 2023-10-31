b6bed14368a2d4df3b4c95dade42a01fb0c93be1
Tim Kettler <tim.kettler@rethinc.de>
2023-10-31 10:59:37 +0100

    Add '--expire' parameter to the 'setexpire' command

--- lib/setexpire_definition.sh
+++ lib/setexpire_definition.sh
@@ -4,5 +4,6 @@ parser_definition_setexpire() {
     msg -- 'Set the expiration date for the given subkey' ''
     msg -- 'Options:'
     param KEYID -k --keyid -- "the keyid to set the expiration for"
+    param KEYEXPIRE -e --expire -- "the expire expression for the key"
     disp :usage -h --help
 }
--- src/setexpire.sh
+++ src/setexpire.sh
@@ -4,6 +4,11 @@ fn_setexpire() {
         exit 1;
     fi
 
+    if [ -z "$KEYEXPIRE" ]; then
+        redprint "Must give the expire expression"
+        exit 1;
+    fi
+
     # Set up trap to ensure settings are restored before exiting
     current_traps=$(trap)
     trap 'fn_cleanup' EXIT HUP INT TERM
@@ -14,5 +19,5 @@ fn_setexpire() {
     MASTER_FPR=$(fn_get_master_fpr)
     KEY_FPR=$(fn_get_key_fpr "${KEYID}")
 
-    gpg --quiet --homedir=$MOUNT_PATH --quick-set-expire ${MASTER_FPR} 10y ${KEY_FPR}
+    gpg --quiet --homedir=$MOUNT_PATH --quick-set-expire ${MASTER_FPR} $KEYEXPIRE ${KEY_FPR}
 }
